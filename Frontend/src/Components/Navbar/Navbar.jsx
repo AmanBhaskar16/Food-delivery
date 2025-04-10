a412547c36a18fedb2d27610ebfbed7d6529c250
AmanBhaskar16 <amanbhaskar16@gmail.com>
2025-04-10 19:40:36 +0530

    auth

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { StoreContext } from '../../context/StoreContext';
 const Navbar = ({setShowLogin}) => {
 
   const [menu,setMenu] =useState("Home");
-  const {getTotalCartAmount} = useContext(StoreContext);
+  const {getTotalCartAmount,token,setToken} = useContext(StoreContext);
 
   return (
     <div className='navbar'>
@@ -25,7 +25,16 @@ const Navbar = ({setShowLogin}) => {
           <div className={getTotalCartAmount()===0?"":"dot"}>
           </div>
         </div>
-        <button onClick={()=>setShowLogin(true)}>Sign In</button>
+        {!token?<button onClick={()=>setShowLogin(true)}>Sign In</button>:
+        <div className='navbar-profile'>
+          <img src={assets.profile_icon} alt="" />
+          <ul className="nav-profile-dropdown">
+            <li><img src={assets.bag_icon} alt="" /> <p>Orders</p></li>
+            <hr />
+            <li><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+          </ul>
+        </div>
+        }
       </div>
     </div>
   )
